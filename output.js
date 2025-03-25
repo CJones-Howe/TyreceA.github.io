1491eae64e3d91c0dc189090512b246fe919553c
TyreceA <Tagbo@albany.edu>
2025-03-24 20:08:55 -0400

    Add files via upload
    uploading output.js file

---
+++ output.js
@@ -0,0 +1,12 @@
+function showInnerHTML() {
+	document.getElementById("output1").innerHTML = "Hello Wlecome to the Page!";
+}
+function useDocumentWrite() {
+	document.write("This replaces the page content! Not recommended.");
+}
+function showAlert() {
+	alert("The sport the US is most known for are Football, Basketball, Baseball, and Soccer!");
+}
+function showConsole() {
+	console.log("In other countries different sports are known compared to the US.");
+	}
